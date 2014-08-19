e995c7b866383fd8b1abac320bd3faea1421d872
Derek Reeve <derek@devbootcamp.com>
2014-08-19 12:14:08 -0700

    Remove unnecessary JSON parsing.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,8 +5,6 @@ require 'json'
 get '/' do
   @emoji_response = HTTParty.get("https://api.github.com/emojis",
                                  headers: {"User-Agent" => "Mule-Deer-App"})
-
-  @emoji_response = JSON.parse(@emoji_response.body)
   erb :index
 end
 
