74360922e9f148a2352e1705de5786f3b3da8d4d
Derek Reeve <derek@devbootcamp.com>
2014-08-18 21:28:53 -0700

    Posting a gist.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,10 +3,28 @@ require 'httparty'
 require 'json'
 
 get '/' do
-
   @emoji_response = HTTParty.get("https://api.github.com/emojis",
                                  headers: {"User-Agent" => "Mule-Deer-App"})
 
   @emoji_response = JSON.parse(@emoji_response.body)
   erb :index
+end
+
+post '/gists' do
+  options = {
+    description: "the description for this gist",
+    public: true,
+    files: {
+      "file1.txt" => {
+        content: "String file contents"
+      }
+    }
+  }
+
+  gist_response = HTTParty.post("https://api.github.com/gists",
+                                 headers: {"User-Agent" => "Mule-Deer-App",
+                                           "Authorization" => "token 31147adbcf010edf2c8d4e8c3f2dfa05d5a172a2"
+                                 },
+                                 body: options.to_json)
+  p gist_response.body
 end
