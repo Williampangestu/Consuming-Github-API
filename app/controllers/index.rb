234dd2938fd277faf496f689c67cea707f096349
William Pangestu <pangestu.william@gmail.com>
2014-09-11 19:20:05 -0700

    Get all the emojis to render successfully

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,6 +3,13 @@ require 'httparty'
 require 'json'
 
 get '/' do
-  @emoji_response = HTTParty.get("https://api.github.com/emojis")
+  @emoji_response = HTTParty.get("https://api.github.com/emojis",
+                    headers: {"User-Agent" => "WilliamPangestu"
+                      })
+  # p @emoji_response
+  # p @emoji_response.body
+  @emojis = JSON.parse(@emoji_response.body)
+  # p @emojis
+  erb :index
 end
 
